26d398996f89474c455665a1d9775fafc4c970d3
srish <Shivansh Srivastava>
2024-08-01 19:39:40 +0530

    Add project files.

---
+++ ProEntity/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProEnity 
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new MyDbContext())
+            {
+                var purchase = new Purchase()
+                {
+                    Id = 1,
+                    Product = "Shirts",
+                    Price = 99.99m
+                };
+                context.Purchases.Add(purchase);
+                context.SaveChanges();
+                var allPurchases=context.Purchases.ToList();
+                var p=context.Purchases.FirstOrDefault(p=>p.Product=="Shirts");
+            }
+        }
+    }
+    public class MyDbContext: DbContext
+    {
+        public DbSet<Purchase> Purchases { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           optionsBuilder.UseInMemoryDatabase("MYDB");
+        }
+    }
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public string? Product { get; set; }
+        public decimal Price { get; set; }
+    }
+}
